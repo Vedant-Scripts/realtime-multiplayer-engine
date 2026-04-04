7f5324508e9c04f1843f92a9ac1504ac6302b5c0
dev-vedant <yvedant13@gmail.com>
2026-04-04 23:13:48 +0530

    feat: react initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+// import { useState } from "react";
+
+// type Board = Array<'X' | 'O' | null>
+
+// const App = () => {
+//   // state of board
+
+//   const [board, setBoard] = useState<Board>(Array(9).fill(null));
+
+//   return (
+//     <div>
+//       <h1> Tic Tac Toe</h1>
+//       <div>
+//         {board.map((val) => (
+//           <p>[]</p>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App;
