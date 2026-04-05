42605cd67bad96a84260a00ef7ebda9451d8f891
dev-vedant <yvedant13@gmail.com>
2026-04-05 23:04:09 +0530

    Board coded

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,40 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// type Board = Array<'X' | 'O' | null>
+type Board = Array<'X' | 'O' | null>
 
-// const App = () => {
-//   // state of board
+const App = () => {
+    // state of board
 
-//   const [board, setBoard] = useState<Board>(Array(9).fill(null));
+    const [board, setBoard] = useState<Board>(Array(9).fill(null));
+    const cellUpdate = (index: number) => {
+        setBoard(board.map((val, ind) => (index === ind ? 'X' : val)));
+    }
+    return (
+        <div>
+            <h1> Tic Tac Toe</h1>
+            <div style={{
+                display: 'grid',
+                gridTemplateColumns: "repeat(3, 50px)",
+                gap: "5px"
+            }} >
+                {board.map((cell, index) => (
+                    <div key={index}
+                        style={{
+                            width: "50px",
+                            height: "50px",
+                            border: "1px solid black",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}
+                        onClick={() => cellUpdate(index)}
+                    >
+                        {cell ?? index}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
 
-//   return (
-//     <div>
-//       <h1> Tic Tac Toe</h1>
-//       <div>
-//         {board.map((val) => (
-//           <p>[]</p>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App;
+export default App;
