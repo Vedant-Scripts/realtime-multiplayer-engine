ed45beb5ed872a84434554e9bcf0c9e23dc87ab3
dev-vedant <yvedant13@gmail.com>
2026-04-06 22:27:08 +0530

    feat: Logic added on fronten, now need to connect to server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,58 @@
 import { useState } from "react";
 
 type Board = Array<'X' | 'O' | null>
+type Player = "X" | "O";
+type BoardValue = Player | null;
 
 const App = () => {
     // state of board
 
     const [board, setBoard] = useState<Board>(Array(9).fill(null));
-    const cellUpdate = (index: number) => {
-        setBoard(board.map((val, ind) => (index === ind ? 'X' : val)));
+    const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>('X');
+    const [status, setStatus] = useState<'playing' | 'finished'>('playing');
+
+    const findWinner = (board: BoardValue[]): Player | null => {
+        if (board.length !== 9) return null;
+        const winningPatterns: Array<[number, number, number]> = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for (const [a, b, c] of winningPatterns) {
+            if (board[a] &&
+                board[a] === board[b] &&
+                board[a] === board[c]
+            ) {
+                return board[a];
+            }
+        }
+        return null;
     }
+
+    const cellUpdate = (index: number) => {
+        if (status !== 'playing') return;
+        if (board[index] !== null) return;
+        const newBoard = board.map((val, ind) =>
+            ind === index ? currentPlayer : val
+        );
+
+        const winner = findWinner(newBoard)
+
+        if (winner) setStatus('finished');
+
+        setBoard(newBoard);
+
+        if (!winner) {
+            setCurrentPlayer(prev => prev === 'X' ? 'O' : 'X');
+        }
+    };
+
     return (
         <div>
             <h1> Tic Tac Toe</h1>
+            <h2>Current Player: {currentPlayer}</h2>
+            {status === "finished" && <h2>Game Over</h2>}
             <div style={{
                 display: 'grid',
                 gridTemplateColumns: "repeat(3, 50px)",
@@ -29,12 +70,13 @@ const App = () => {
                         }}
                         onClick={() => cellUpdate(index)}
                     >
-                        {cell ?? index}
+                        {cell ?? ""}
                     </div>
                 ))}
             </div>
         </div>
     )
 }
+
 
 export default App;
